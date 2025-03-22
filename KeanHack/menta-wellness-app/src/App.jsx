48b0992126aa03e9c3e4503e51ee039547bc0d6e
Raphael Holganza <uchicoi1228@gmail.com>
2025-03-22 15:22:21 -0400

    Adding banner

--- KeanHack/menta-wellness-app/src/App.jsx
+++ KeanHack/menta-wellness-app/src/App.jsx
@@ -7,6 +7,10 @@ function App() {
   const [count, setCount] = useState(0);
   return (
     <>
+      {/* Banner */}
+      <header className="w-full bg-[#9CBA7F] text-white text-2xl font-bold py-4 text-center">
+        Urban Wellness Locator
+      </header>
       <MapComponent />
       <div className="mt-5">
         <CategoryButtons />
